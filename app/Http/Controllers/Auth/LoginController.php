5a4bc54aa4cd6bd3ae173902f8bf13ab6f2b9e95
Juan Jesús Fernández Rodríguez <alba-juanjesus@hotmail.es>
2022-10-26 14:07:27 +0200

    26-10-22_14-07
    Update LoginController.php, intentando hacer la selección de si la cuenta está activada o no

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,12 +20,35 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+
+            if(Auth::attempt('active' == 1)){
+                $request->session()->regenerate();
+                return redirect()->intended('dashboard');
+            }
+            return back()->withErrors([
+                'email' => 'The provided user do not have activate the account.',
+            ])->onlyInput('email');
+            
+        }
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
