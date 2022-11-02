a17a3e27a3d60f1281ebe0a04e969d3614e86eb8
Juan Jesús Fernández Rodríguez <alba-juanjesus@hotmail.es>
2022-11-02 09:17:33 +0100

    02/11/22 - 9:13
    Funcionalidad Login + modificación del databaseseeder para que se encripte la contraseña del administrador y darle tipo a ( administrador ).

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,14 +30,8 @@ class LoginController extends Controller
         ]);
  
         if (Auth::attempt($credentials)) {
-
-            if(Auth::attempt('active' == 1)){
-                $request->session()->regenerate();
-                return redirect()->intended('dashboard');
-            }
-            return back()->withErrors([
-                'email' => 'The provided user do not have activate the account.',
-            ])->onlyInput('email');
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
             
         }
         return back()->withErrors([
@@ -50,7 +44,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    //protected $redirectTo = '/home';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -61,4 +55,46 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(\Illuminate\Http\Request $request) {
+        $this->validateLogin($request);
+    
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+
+            // This section is the only change
+            if ($this->guard()->validate($this->credentials($request))) {
+                $user = $this->guard()->getLastAttempted();
+        
+                // Make sure the user is active
+                if ($user->actived && $this->attemptLogin($request)) {
+                    // Send the normal successful login response
+                    return $this->sendLoginResponse($request);
+                } else {
+                    // Increment the failed login attempts and redirect back to the
+                    // login form with an error message.
+                    $this->incrementLoginAttempts($request);
+                    return redirect()
+                    -> route('login')
+                    ->with('actived','Administrator must active your account.');
+                }
+            } else{
+        
+                // If the login attempt was unsuccessful we will increment the number of attempts
+                // to login and redirect the user back to the login form. Of course, when this
+                // user surpasses their maximum number of attempts they will get locked out.
+                $this->incrementLoginAttempts($request);
+            
+                return $this->sendFailedLoginResponse($request);
+
+            }
+        
+    }
+
 } 
