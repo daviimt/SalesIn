121b11be09cd0f33bfd27ff54efc5ec471b3a9cf
daviimt <dmateomerino@gmail.com>
2022-11-21 10:37:51 +0100

    creacion de article controller

--- routes/web.php
+++ routes/web.php
@@ -31,12 +31,12 @@ Auth::routes();
 Route::get('/email/verify/{id}','AdminController@verificate_email');
 Route::get('/home', 'HomeController@index')->name('home');
 
-
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/admin','AdminController@index');
     //CRUD
     Route::get('/users/{user}','AdminController@show')->name('users.show');
     Route::get('/users/{user}/edit','AdminController@edit')->name('users.edit');
+    Route::get('/articles','ArticleController@index');
     Route::put('/users/{user}/activate','AdminController@activate')->name('users.activate');
     Route::put('/users/{user}/desactivate','AdminController@desactivate')->name('users.desactivate');
     Route::put('/users/{user}/softdel','AdminController@softdel')->name('users.softdel');
