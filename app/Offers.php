d2ec6e133888513045e4a34b428035c2eb98fcb7
daviimt <dmateomerino@gmail.com>
2023-01-11 11:33:54 +0100

    cambiar view a user y apply

--- app/Offers.php
+++ app/Offers.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Offers extends Model
 {
     protected $table='offers';
+
+    protected $fillable=['id','title','description','date_max','num_candidates','deleted'];
 }
+
+
