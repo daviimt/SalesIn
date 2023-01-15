a4970bc1406c22af31c9caceb9499a7029381ef7
Chette <80092129+MrChette@users.noreply.github.com>
2023-01-15 17:29:33 +0100

    faltan las imagenes en el pdf

--- app/Http/Controllers/InformesController.php
+++ app/Http/Controllers/InformesController.php
@@ -8,6 +8,7 @@ use App\Articles;
 use App\Cicles;
 use App\Offers;
 use App\Applied;
+use App\User;
 
 
 
@@ -18,6 +19,7 @@ class InformesController extends Controller
         $offers = Offers::all();
         $cicles = Cicles::all(); 
         $user_id= auth()->id();
+        $user = User::find($user_id);
 
         $offers = Offers::select('offers.id', 'offers.title', 'offers.description','offers.cicle_id', 'offers.num_candidates', 'offers.created_at', 'offers.updated_at', 'offers.deleted', 'applieds.offer_id', 'applieds.user_id')
                 ->leftJoin('applieds', function($join) use ($user_id) {
@@ -25,7 +27,7 @@ class InformesController extends Controller
                       ->where('applieds.user_id', '=', $user_id);
               })->whereNotNull('applieds.id')->where('offers.deleted', 0)->get();
 
-        $pdf = \PDF::loadView('general',compact('offers',$offers));;
+        $pdf = \PDF::loadView('general',compact('offers',$offers,'cicles',$cicles,'user',$user));;
         // Para crear un pdf en el navegador usaremos la siguiente línea
         return $pdf->stream();
         // Para descargar un pdf en un archivo usaremos la siguiente línea
--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -1,5 +1,30 @@
 <div class="content">
     @foreach ($offers as $offer)
-        <p>{{ $offer->title }}</p>
+        @if($offer->deleted == 0)
+            @foreach ($cicles as $cicle)
+                @if($offer->cicle_id == $cicle->id) 
+                    @if(str_contains($cicle->img, ".png"))
+                        <p> {{ asset('images/'.$cicle->img) }} </p>
+                        @else
+                        <p>{{ asset('images/noimage.png') }}" </p>
+                    @endif
+                @endif
+            @endforeach
+        @endif
     @endforeach
+
+    <br/>
+    <div name="userInfo" style="margin-left:60%">
+        <p> Nombre : {{$user->name}}</p>
+        <p> Apellido : {{$user->surname}}</p>   
+    </div>
+    <br/>
+
+    
+    @forelse($offers as $offer)
+        <p>{{ $offer->title }}: {{$offer->description }}</p>
+        @empty
+            <p>No hay ofertas aplicadas</p>
+    @endempty
+    
 </div>
